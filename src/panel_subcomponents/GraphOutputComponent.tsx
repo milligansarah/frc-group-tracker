41cff129b054851e03d4e88f3c224a47bda5d06c
milligansarah <98560997+milligansarah@users.noreply.github.com>
2023-12-26 21:10:25 -0500

    Fixed graph scale, removed extra labels in legend, and styled hover effect

--- src/panel_subcomponents/GraphOutputComponent.tsx
+++ src/panel_subcomponents/GraphOutputComponent.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import { Bar, CartesianGrid, ComposedChart, ErrorBar, Legend, Line, LineChart, RectangleProps, Tooltip, XAxis, YAxis, ZAxis } from "recharts";
+import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, useState } from "react";
+import { Bar, CartesianGrid, ComposedChart, DotProps, ErrorBar, Legend, Line, LineChart, RectangleProps, Tooltip, XAxis, YAxis, ZAxis } from "recharts";
 import queryType from "../QueryType";
 import { CircularProgress } from "@mui/material";
-import { ArrowDropDown, ArrowDropUp, ArrowUpward, CircleOutlined } from "@mui/icons-material";
+import '../index.css';
 
 const tealColor = '#3BBA9C'
+const tealColorClear = '#3BBA9CAA'
 type teamAPIRequestType = {
     "event_points": [],
     "point_total": number,
@@ -62,13 +63,18 @@ function GraphOutputComponent(props: queryType) {
                     standardDeviation += Math.pow((ranks[rankIndex] - mean), 2)
                 }
                 standardDeviation = Math.sqrt(standardDeviation / (ranks.length - 1))
+
                 newData.push({
                     Year: year,
-                    mean: Number(mean.toFixed(2)),
                     median: Number(median!.toFixed(2)),
-                    sd: standardDeviation,
-                    bottomWhisker: mean - (2 * standardDeviation)
+                    mean: Number(mean.toFixed(2)),
+                    min: mean - standardDeviation * 2,
+                    bottomWhisker: standardDeviation,
+                    bottomBox: standardDeviation, 
+                    topBox: standardDeviation,
+                    topWhisker: standardDeviation, 
                 })
+
                 setNewData(newData)
             }
         }
@@ -82,27 +88,6 @@ function GraphOutputComponent(props: queryType) {
     }
 
     fetchData()
-
-    const WhiskerLine = (props: RectangleProps) => {
-        const { x, y, width, height } = props;
-      
-        if (x == null || y == null || width == null || height == null) {
-          return null;
-        }
-      
-        return (
-          <line
-            x1={x + width / 2}
-            y1={y + 2 * height}
-            x2={x + width / 2}
-            y2={y + height}
-            stroke={tealColor}
-            strokeWidth={2}
-            opacity={"50%"}
-            fillOpacity={"0%"}
-          />
-        );
-    };
     
     const BarOutline = (props: RectangleProps) => {
         const { x, y, width, height } = props;
@@ -113,8 +98,10 @@ function GraphOutputComponent(props: queryType) {
         
         return (
             <rect
-              x={x + 2 * width / 6}
-              y={y + height}
+              x={x+1*width/3}
+              y={y}
+              x1={x+width}
+              y1={y+height}
               width={width / 3}
               height={height}
               stroke={tealColor}
@@ -126,42 +113,105 @@ function GraphOutputComponent(props: queryType) {
         );
     };
 
-    const TopWhisker = (props: RectangleProps) => {
+    const DotBar = (props: RectangleProps) => {
         const { x, y, width, height } = props;
       
         if (x == null || y == null || width == null || height == null) {
           return null;
         }
       
-        return <line x1={x + 2 * width / 3} y1={y + 2*height} x2={x + width / 3} y2={y + 2*height} stroke={tealColor} opacity={"50%"} strokeWidth={2} />;
-    };
-
-    const BottomWhisker = (props: RectangleProps) => {
+        return (
+          <line
+            x1={x + width / 2}
+            y1={y}
+            x2={x + width / 2}
+            y2={y + height}
+            stroke={tealColor}
+            strokeWidth={2}
+            opacity={"50%"}
+            fillOpacity={"0%"}
+          />
+        );
+      };
+      
+    const HorizonBar = (props: RectangleProps) => {
         const { x, y, width, height } = props;
       
         if (x == null || y == null || width == null || height == null) {
           return null;
         }
       
-        return <line x1={x + 2 * width / 3} y1={y + height} x2={x + width / 3} y2={y + height} stroke={tealColor} opacity={"50%"} strokeWidth={2} />;
+        return <line 
+            x1={x + 1 * width / 3} 
+            y1={y} 
+            x2={x + 2 *width / 3} 
+            y2={y + height} 
+            stroke={tealColor}
+            strokeWidth={2}
+            opacity={"50%"}
+            fillOpacity={"0%"} 
+        />;
     };
 
-    return show ? <CircularProgress id="loading-icon"/>: <ComposedChart width={550} height={550} data={newData}>
-        <Legend iconType="plainline" fontFamily="Arial, Helvetica, sans-serif" fontSize={12}/>
-        <XAxis fontFamily="Arial, Helvetica, sans-serif" fontSize={11} strokeWidth={3} stroke="#EEEEEE" dataKey="Year" tickLine={false} />
-        <CartesianGrid opacity={"50%"} stroke="#EEEEEE" />
-        <YAxis fontFamily="Arial, Helvetica, sans-serif" fontSize={11} strokeWidth={3} stroke="#EEEEEE" tickLine={false}/>
-        <Tooltip contentStyle={{backgroundColor: "#FF000000", border: "none"}} labelStyle={{fontSize: 14}} itemStyle={{fontSize: 14, fontFamily: "Arial, Helvetica, sans-serif", color: "#EEEEEE", lineHeight: 0.5}} />
-        <Line type="monotone" dataKey="mean" stroke={tealColor} strokeWidth={2}/>
-        <Line type="monotone" dataKey="median" stroke={tealColor} strokeWidth={2} strokeDasharray='4, 8, 12, 16'/>
-        <Bar stackId={'a'} dataKey={'bottomWhisker'} fill={'none'} />
-        <Bar activeBar={<BottomWhisker/>} stackId={'a'} dataKey={'sd'} shape={<BottomWhisker/>} />
-        <Bar activeBar={<WhiskerLine/>} stackId={'a'} dataKey={'sd'} shape={<WhiskerLine/>}/>
-        <Bar activeBar={<BarOutline/>} stackId={'a'} dataKey={'sd'} fillOpacity={"50%"} shape={<BarOutline/>}/>
-        <Bar activeBar={<BarOutline/>} stackId={'a'} dataKey={'sd'} fillOpacity={"50%"} shape={<BarOutline/>}/>
-        <Bar activeBar={<WhiskerLine/>} stackId={'a'} dataKey={'sd'} shape={<WhiskerLine/>} />
-        <Bar activeBar={<TopWhisker/>} stackId={'a'} dataKey={'sd'} shape={<TopWhisker/>} />
-    </ComposedChart>
+    const CustomTooltip = ({ active, payload, label } : any) => {
+        if (active && payload && payload.length) {
+          return (
+            <div id="custom-tooltip">
+              <p>{label}</p>
+              <p>Mean Percentile Rank: {payload[0].value}</p>
+              <p>Median Percentile Rank: {payload[1].value}</p>
+              <p>Standard Deviation From Mean: {payload[3].value.toFixed(2)}</p>
+            </div>
+          );
+        }
+        
+        return null;
+    };
+
+    const ActiveDot = (props: any) => {
+        const { cx, cy } = props;
+
+        return (
+            <circle cx={cx} cy={cy} r={8} stroke={tealColorClear} strokeWidth={2} fill="transparent" />
+        );
+    };
+
+    const CustomDot = (props: any) => {
+        const { cx, cy } = props;
+
+        return (
+            <circle cx={cx} cy={cy} r={5} fill={tealColorClear} />
+        );
+    };
+
+    return show ? <CircularProgress id="loading-icon"/>: <div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+                <div style={{display: 'flex', alignItems: "center", marginRight: 30}}>
+                    <p style={{fontSize: 12, marginRight: 10}}>Mean Percentile Rank</p>
+                    <div className="line"></div>
+                </div>
+                <div style={{display: 'flex', alignItems: "center"}}>
+                    <p style={{fontSize: 12, marginRight: 10}}>Median Percentile Rank</p>
+                    <div className="line" id="dashed-line"></div>
+                </div>
+        </div>
+        <ComposedChart width={550} height={550} data={newData}>
+            <XAxis fontFamily="Arial, Helvetica, sans-serif" fontSize={11} strokeWidth={3} stroke="#EEEEEE" dataKey="Year" tickLine={false} />
+            <CartesianGrid opacity={"50%"} stroke="#EEEEEE" />
+            <YAxis fontFamily="Arial, Helvetica, sans-serif" fontSize={11} strokeWidth={3} stroke="#EEEEEE" tickLine={false}/>
+            <Tooltip content={<CustomTooltip/>} contentStyle={{backgroundColor: "#FF000000", border: "none"}} labelStyle={{fontSize: 14}} itemStyle={{fontSize: 14, fontFamily: "Arial, Helvetica, sans-serif", color: "#EEEEEE", lineHeight: 0.5}} />
+            <Line type="monotone" dataKey="mean" stroke={tealColor} strokeWidth={2} activeDot={<ActiveDot/>} dot={<CustomDot/>}/>
+            <Line type="monotone" dataKey="median" stroke={tealColor} strokeWidth={2} strokeDasharray='4, 2' activeDot={<ActiveDot/>} dot={<CustomDot/>}/>
+            <Bar stackId={'a'} dataKey={'min'} fill={'none'} legendType="none" activeBar={false}/>
+            <Bar stackId={'a'} dataKey={'bar'} shape={<HorizonBar />} legendType="none" activeBar={false}/>
+            <Bar stackId={'a'} dataKey={'bottomWhisker'} shape={<DotBar />} legendType="none" activeBar={false}/>
+            <Bar stackId={'a'} dataKey={'bottomBox'} shape={<BarOutline/>} legendType="none" activeBar={false}/>
+            <Bar stackId={'a'} dataKey={'topBox'} shape={<BarOutline/>} legendType="none" activeBar={false}/>
+            <Bar stackId={'a'} dataKey={'topWhisker'} shape={<DotBar />} legendType="none" activeBar={false}/>
+            <Bar stackId={'a'} dataKey={'bar'} shape={<HorizonBar />} legendType="none" activeBar={false}/>
+        <ZAxis type='number' dataKey='size' range={[0, 250]} />
+        </ComposedChart>
+    </div>
 }
 
 export default GraphOutputComponent
